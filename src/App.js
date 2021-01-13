639d6a297d6a593d34e2405201bcb7ab35fdc145
BostonWafro <wafrogaming@gmail.com>
2021-01-13 10:50:59 +0000

    started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  state = {
+    persons: [
+      {name: "Dan", age: "34"},
+      {name: "Ben", age: "21"}
+    ]
+  }
+  render() {
+    return (
+      <div className="App"> 
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+      </div>
+    );
+  }
+}
+
+const Person = (props) => {
+  return(
+    <div>
+      <h1>Hi my name is {props.name} and I am {props.age} years old</h1>
     </div>
-  );
+  )
 }
 
 export default App;
